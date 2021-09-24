4b3fe488961a95b920d8304778dd0bade812c2d8
johannpereze <sebas.jsp@gmail.com>
2021-09-24 13:36:54 -0500

    Creo la class del server y sirvo contenido estatico de la carpeta punlic

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hola mundo");
+require("dotenv").config();
+
+const Server = require("./models/server");
+
+const server = new Server();
+
+server.listen();
---
+++ models/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // middlewares
+    this.middlewares();
+
+    // Rutas de mi app
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.status(201).json({
+        msg: "get API",
+      });
+    });
+    this.app.put("/api", (req, res) => {
+      res.json({
+        msg: "put API",
+      });
+    });
+    this.app.post("/api", (req, res) => {
+      res.json({
+        msg: "post API",
+      });
+    });
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        msg: "delete API",
+      });
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Listening on http://localhost:${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
